2a34e8e5ab6c6eb10ef040ea04d68a8589508366
Farid <farid6566@gmail.com>
2023-11-02 14:17:41 +0500

    First commit

---
+++ movie/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Movie
+
+def home(request):
+    searchTerm = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+
+
+def about(request):
+    return HttpResponse('<h1> Welcome to About Page</h1>')
+
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email':email})
+# Create your views here.
