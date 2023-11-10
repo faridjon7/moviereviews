42604df21994e9fabb32f62037d00a88160863e5
Farid <farid6566@gmail.com>
2023-11-10 12:33:16 +0500

    title

--- movie/models.py
+++ movie/models.py
@@ -5,5 +5,9 @@ class Movie(models.Model):
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='movie/images/')
     url = models.URLField(blank=True)
+    
+    def __str__(self):
+        return self.title
+
 
 # Create your models here.
